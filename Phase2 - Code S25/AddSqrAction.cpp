835afc3b6519456c1e3b426cd24730a6d57bf4d6
Maya Aboshadi <mayaaboshadii@gmail.com>
2025-05-03 18:30:35 +0300

    commit

--- Phase2 - Code S25/AddSqrAction.cpp
+++ Phase2 - Code S25/AddSqrAction.cpp
@@ -6,8 +6,6 @@
 #include "..\GUI\input.h"
 #include "..\GUI\Output.h"
 
-using namespace std;
-
 AddSqrAction::AddSqrAction(ApplicationManager* pApp) :Action(pApp)
 {
 }
@@ -18,36 +16,34 @@ void AddSqrAction::ReadActionParameters()
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
 
-	pOut->PrintMessage("New Square: Click on centre");
+	pOut->PrintMessage("New Rectangle: Click at first corner");
 
-	//Read centre and store in point P1
+	//Read 1st corner and store in point P1
 	pIn->GetPointClicked(P1.x, P1.y);
 
-	pOut->PrintMessage("New Square: Enter length");
+	pOut->PrintMessage("New Rectangle: Click at second corner");
 
-	//Read length using stod() function
-	//unsure of how to get length
-	Length = stod(pIn->GetSrting(pOut));
+	//Read 2nd corner and store in point P2
+	pIn->GetPointClicked(P2.x, P2.y);
 
-	SqrGfxInfo.isFilled = false;	//default is not filled
+	RectGfxInfo.isFilled = false;	//default is not filled
 	//get drawing, filling colors and pen width from the interface
-	SqrGfxInfo.DrawClr = pOut->getCrntDrawColor();
-	SqrGfxInfo.FillClr = pOut->getCrntFillColor();
-
+	RectGfxInfo.DrawClr = pOut->getCrntDrawColor();
+	RectGfxInfo.FillClr = pOut->getCrntFillColor();
 
 	pOut->ClearStatusBar();
 
 }
 
 //Execute the action
-void AddSqrAction::Execute()
+void AddRectAction::Execute()
 {
 	//This action needs to read some parameters first
 	ReadActionParameters();
 
-	//Create a square with the parameters read from the user
-	CSquare* S = new CSquare(P1, Length, SqrGfxInfo);
+	//Create a rectangle with the parameters read from the user
+	CRectangle* R = new CRectangle(P1, P2, RectGfxInfo);
 
-	//Add the square to the list of figures
-	pManager->AddFigure(S);
+	//Add the rectangle to the list of figures
+	pManager->AddFigure(R);
 }
