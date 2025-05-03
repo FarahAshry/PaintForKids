a2a0b57dc2ca8c8bcb0d26a4e9c4231069a8964a
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-04 00:12:08 +0300

    Create Missing.h

---
+++ Phase2 - Code S25/Actions/Missing.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Actions/Action.h"
+#include "Figures/CFigure.h"
+
+class Missing : public Action
+{
+private:
+	ApplicationManager* pManager;
+	Point P1; 
+	CFigure* fig;
+	int score; 
+	bool hidden;
+
+public:
+	Missing(ApplicationManager* pApp, int score);
+	void ReadActionParameters();
+	void Execute();
+	void incScore();
+	void decScore();
+	int getScore();
+	void HideFigure(CFigure& fig, bool hide);
+	void ShowFigure(CFigure& fig, bool show);
+	bool isHidden();
+	~Missing();
+};
+
