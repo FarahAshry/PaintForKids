493b4973cb72e9152171c814efefe0349ddbf77d
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-03 23:28:58 +0300

    Update Matching.cpp

--- Phase2 - Code S25/Matching.cpp
+++ Phase2 - Code S25/Matching.cpp
@@ -1,42 +1,59 @@
 #include "Matching.h"
-#include "D:\Uni\Semester 2\Programming Techniques\Project\PT Project Phase1 - Code S25\Phase2 - Code S25\Figures\CFigure.h"
-#include "D:\Uni\Semester 2\Programming Techniques\Project\PT Project Phase1 - Code S25\PT Project Phase1 - Code S25\Phase2 - Code S25\Figures\CRectangle.h"
+#include "Figures\CFigure.h"
+#include "Figures\CRectangle.h"
 #include "ApplicationManager.h"
-#include "D:\Uni\Semester 2\Programming Techniques\Project\PT Project Phase1 - Code S25\Phase2 - Code S25\GUI\Output.h"
-#include "D:\Uni\Semester 2\Programming Techniques\Project\PT Project Phase1 - Code S25\Phase2 - Code S25\GUI\Input.h"
+#include "GUI\Output.h"
+#include "GUI\Input.h"
 
-Matching::Matching(ApplicationManager* pApp, int score) :Action(pApp), pManager(pApp), score(0), fig1(nullptr), fig2(nullptr) {
+Matching::Matching(ApplicationManager* pApp, int score) :Action(pApp), pManager(pApp), score(0) {
 	this->pManager = pApp;
-	CRectangle* rect = pManager->GetFigure(P1.x, P1.y);
-
+	
 }
 
 void Matching::ReadActionParameters() {
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
+
 	pOut->PrintMessage("Matching Game: Click on the first figure.");
 	pIn->GetPointClicked(P1.x, P1.y); // Get the first click
 
 	pOut->PrintMessage("Matching Game: Click on the second figure.");
 	pIn->GetPointClicked(P2.x, P2.y); // Get the second click
+
 	pOut->ClearStatusBar();
 }
 void Matching::Execute() {
+
 	ReadActionParameters();
+	fig1 = pManager->GetFigure(P1.x, P1.y);
+	fig2 = pManager->GetFigure(P2.x, P2.y);
+
+	Output* pOut = pManager->GetOutput();
+
+	compare(*fig1, *fig2); 
+
 }
 
 void Matching::compare(CFigure& fig1, CFigure& fig2) {
 		Output* pOut = new Output;
-	if (fig1.getColor() == fig2.getColor() ) {
+		bool match = false;
+		if (fig1.getColor() == fig2.getColor()) {
+			match = true;
+		}
+	if (match==true ) {
 		pOut->PrintMessage("Match found!");
-		incscore();
+		incScore();
 	}
 	else {
 		pOut->PrintMessage("Match not found!");
+		decScore();
 	}
 }
-void Matching::incscore() {
+void Matching::incScore() {
 	score++;
+}
+void Matching::decScore() {
+	score--;
 }
 int Matching::getScore() {
 	return score;
