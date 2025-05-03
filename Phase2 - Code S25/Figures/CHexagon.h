88a5a164f87ccc24ce818f319a864a8887cc5259
Maya Aboshadi <mayaaboshadii@gmail.com>
2025-05-03 14:00:51 +0300

    CHexagon in figures

---
+++ Phase2 - Code S25/Figures/CHexagon.h
@@ -0,0 +1,17 @@
+#ifndef CHEX_H
+#define CHEX_H
+
+#include "CFigure.h"
+
+class CHexagon : public CFigure
+{
+private:
+	Point centre;
+	double length;
+public:
+	CHexagon(Point, double, GfxInfo FigureGfxInfo);
+	virtual void Draw(Output* pOut) const;
+	CFigure* Clone() const;
+};
+
+#endif
