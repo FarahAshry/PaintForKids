6bbcc698a14ea52225d684776bdd71e12b6c4098
Maya Aboshadi <mayaaboshadii@gmail.com>
2025-05-03 17:57:39 +0300

    square

--- Phase2 - Code S25/AddSqrAction.h
+++ Phase2 - Code S25/AddSqrAction.h
@@ -1,5 +1,24 @@
 #pragma once
-class AddSqrAction
+#ifndef ADD_SQR_ACTION_H
+#define ADD_SQR_ACTION_H
+
+#include "Action.h"
+
+//Add Square Action class
+class AddSqrAction : public Action
 {
+private:
+	Point centre; //Square centre
+	double length; //Length of sides
+	GfxInfo SqrGfxInfo;
+public:
+	AddSqrAction(ApplicationManager* pApp);
+
+	//Reads square parameters
+	virtual void ReadActionParameters();
+
+	//Add rectangle to the ApplicationManager
+	virtual void Execute();
 };
 
+#endif
