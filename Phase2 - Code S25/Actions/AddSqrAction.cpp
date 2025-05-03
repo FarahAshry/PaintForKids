b9d39db6fc5629743cc09bf0f934162ba4116fd3
Maya Aboshadi <mayaaboshadii@gmail.com>
2025-05-03 18:52:15 +0300

    square action

---
+++ Phase2 - Code S25/Actions/AddSqrAction.cpp
@@ -0,0 +1,52 @@
+#include "AddSqrAction.h"
+#include "..\Figures\CSquare.h"
+
+#include "..\ApplicationManager.h"
+
+#include "..\GUI\input.h"
+#include "..\GUI\Output.h"
+
+using namespace std;
+
+AddSqrAction::AddSqrAction(ApplicationManager* pApp) :Action(pApp)
+{
+}
+
+void AddSqrAction::ReadActionParameters()
+{
+	//Get a Pointer to the Input / Output Interfaces
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+
+	pOut->PrintMessage("New Square: Click on centre");
+
+	//Read centre of square and place in P1
+	pIn->GetPointClicked(P1.x, P1.y);
+
+	pOut->PrintMessage("New Square: Enter length");
+
+	//Read length of the square
+	Length = stod(pIn->GetSrting(pOut));
+
+	SqrGfxInfo.isFilled = false;	//default is not filled
+	//get drawing, filling colors and pen width from the interface
+	SqrGfxInfo.DrawClr = pOut->getCrntDrawColor();
+	SqrGfxInfo.FillClr = pOut->getCrntFillColor();
+
+	pOut->ClearStatusBar();
+
+}
+
+//Execute the action
+void AddSqrAction::Execute()
+{
+	//This action needs to read some parameters first
+	ReadActionParameters();
+
+	//Create a square with the parameters read from the user
+	CSquare* S = new CSquare(P1, Length, SqrGfxInfo);
+
+	//Add the square to the list of figures
+	pManager->AddFigure(S);
+}
+
---
+++ Phase2 - Code S25/Actions/AddSqrAction.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef ADD_SQR_ACTION_H
+#define ADD_SQR_ACTION_H
+
+#include "Action.h"
+
+//Add Square Action class
+class AddSqrAction : public Action
+{
+private:
+	Point P1; //Square centre
+	double Length; //Square length
+	GfxInfo SqrGfxInfo;
+public:
+	AddSqrAction(ApplicationManager* pApp);
+
+	//Reads square parameters
+	virtual void ReadActionParameters();
+
+	//Add square to the ApplicationManager
+	virtual void Execute();
+};
+
+#endif
