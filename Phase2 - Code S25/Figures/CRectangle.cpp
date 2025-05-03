d242e44fce36e3421bab8f2bdbfbd6c84e246780
daiKhaled <dai.abdelsalam07@eng-st.cu.edu.eg>
2025-05-04 00:20:39 +0300

    Update CRectangle.cpp

--- Phase2 - Code S25/Figures/CRectangle.cpp
+++ Phase2 - Code S25/Figures/CRectangle.cpp
@@ -19,3 +19,17 @@ void CRectangle::Load(ifstream& Infile) {
  CFigure* CRectangle::Clone() const {
 	return new CRectangle(*this);
 }
+void CRectangle::paste_clone(Point c)
+{
+ int N_X = ((Corner1.x + Corner2.x) / 2);
+ int N_Y = ((Corner1.y + Corner2.y) / 2);
+ int P_X = c.x - N_X;
+ int P_Y = c.y - N_Y;
+
+ Corner1.x = Corner1.x + P_X;
+ Corner2.x = Corner2.x + P_X;
+ Corner1.y = Corner1.x + P_Y;
+ Corner2.y = Corner2.x + P_Y;
+
+
+}
