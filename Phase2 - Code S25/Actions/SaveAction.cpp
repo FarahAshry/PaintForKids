7f83165cd490032b9d8104f6e8ab4a9efe4ab674
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-04 23:33:43 +0300

    Update SaveAction.cpp

--- Phase2 - Code S25/Actions/SaveAction.cpp
+++ Phase2 - Code S25/Actions/SaveAction.cpp
@@ -32,10 +32,7 @@ void SaveAction::Execute(bool readparameters)
 	else
 		file << (pOut->getCrntDrawColor()) << "  " << (pOut->getCrntFillColor()) << endl;
 
-	for (int i = 0; i < /*figure count*/; i++)
-	{
-		FigList[i]->Save(file);
-	}
+	pManager->saveAll();
 	file.close();
 }
 
