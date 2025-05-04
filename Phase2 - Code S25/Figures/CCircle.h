33116c8bda5b993988225eab568ce091c63613fa
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-05 00:37:22 +0300

    Update CCircle.h

--- Phase2 - Code S25/Figures/CCircle.h
+++ Phase2 - Code S25/Figures/CCircle.h
@@ -14,7 +14,7 @@ virtual void Draw(Output* pOut) const;
 virtual void Load(ofstream& Infile);
 virtual CFigure* Clone() const;
 virtual void paste_clone(Point c);
-void save(ofstream& OutFile) const;
+virtual void save(ofstream& OutFile) const;
 };
 
 #endif
