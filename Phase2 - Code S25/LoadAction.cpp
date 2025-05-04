3329d9454ab6411e90a2c7ce5dd4281c0ce699cf
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-04 23:39:03 +0300

    Update LoadAction.cpp

--- Phase2 - Code S25/LoadAction.cpp
+++ Phase2 - Code S25/LoadAction.cpp
@@ -4,41 +4,79 @@
 #include "GUI\Output.h"
 #include "GUI\Input.h"
 #include <iostream>
+#include "Figures\CRectangle.h"
+#include "Figures/CSquare.h" 
+#include "Figures/CTriangle.h" 
+#include "Figures/CHexagon.h"
+#include "Figures/CCircle.h"
 #include <fstream>
+#include "GUI\UI_Info.h"
+#include "DEFS.h"
 using namespace std;
 
-LoadAction::LoadAction(ApplicationManager* pApp, int incFig) :Action(pApp) {
-	this->pManager = pApp;
-	LoadFile.open("Figures.txt", ios::out);
-	if (!LoadFile.is_open()) {
-	    cout << "Error opening file" << endl;
-	}
-	else {
-		ActionType ActType;
-		cout << "File opened successfully" << endl;
-		LoadFile << incFig << endl;
-		for (int i = 0; i < incFig; i++) {
-			CFigure* pFig = pManager->GetFigure(P1.x,P1.y);
-			if (pFig != nullptr) {
-				pFig->Load(LoadFile);
-				pFig->setID(i);
-				LoadFile <<pFig pFig->getID() <<"\t";
-			}
-		}
-	}
-}
+LoadAction::LoadAction(ApplicationManager* pApp) :Action(pApp) {}
+
 void LoadAction::ReadActionParameters() {
-	//Get a Pointer to the Input / Output Interfaces
+	
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
 	pOut->PrintMessage("Load Figures: Click to load");
 	pIn->GetPointClicked(P1.x, P1.y);
 	pOut->ClearStatusBar();
 }
+
+
+
 void LoadAction::Execute() {
+
+	string FigColor;
+
+
 	//This action needs to read some parameters first
 	ReadActionParameters();
-	
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+	string tempColor;
+
+	LoadFile.open("Figures.txt", ios::out);
+	if (!LoadFile.is_open()) {
+		cout << "Error opening file" << endl;
+	}
+	else {
+		
+		cout << "File opened successfully" << endl;
+		LoadFile << incFig << endl;
+		for (int i = 0; i < 200; i++) {
+			CFigure* pFig = pManager->GetFigure(P1.x, P1.y);
+			
+			if (pFig != nullptr) {
+				if (pFig->getColor() == RED) {
+					tempColor = "Red";
+				}
+				else if (pFig->getColor() == BLUE) {
+					tempColor = "Blue";
+				}
+				else if (pFig->getColor() == YELLOW) {
+					tempColor = "Yellow";
+				}
+				else if (pFig->getColor() == BLACK) {
+					tempColor = "Black";
+				}
+				else if (pFig->getColor() == ORANGE) {
+					tempColor = "Orange";
+				}
+				else if (pFig->getColor() == GREEN) {
+					tempColor = "Green";
+				}
+
+				pFig->Load(LoadFile);
+				pFig->setID(i);
+				LoadFile << pFig->getFigureType()<< "\t" << pFig->getID() << endl;
+
+			}
+
+		}
+	}
 }
 LoadAction::~LoadAction() {
 	LoadFile.close();
