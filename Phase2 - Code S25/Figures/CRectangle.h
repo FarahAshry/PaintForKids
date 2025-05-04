d1d0191e8b6d573e0ddabc697a57f00e1b049c9d
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-05 02:28:00 +0300

    Update CRectangle.h

--- Phase2 - Code S25/Figures/CRectangle.h
+++ Phase2 - Code S25/Figures/CRectangle.h
@@ -12,7 +12,7 @@ public:
 	CRectangle(Point, Point, GfxInfo FigureGfxInfo);
 	virtual void Draw(Output* pOut) const;
 	FigureType getFigureType() const;
-	virtual void Load(ofstream& Infile);
+	virtual void Load(ifstream& Infile);
 	virtual CFigure* Clone() const;
 	virtual void paste_clone(Point c);
 	virtual void save(ofstream& OutFile) const;
