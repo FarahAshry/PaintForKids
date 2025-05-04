b600bc931736c3b6220068459929990d36e8d46a
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-05 02:21:23 +0300

    Update LoadAction.cpp

--- Phase2 - Code S25/LoadAction.cpp
+++ Phase2 - Code S25/LoadAction.cpp
@@ -3,12 +3,12 @@
 #include "Figures\CFigure.h"
 #include "GUI\Output.h"
 #include "GUI\Input.h"
-#include <iostream>
-#include "Figures\CRectangle.h"
-#include "Figures/CSquare.h" 
-#include "Figures/CTriangle.h" 
-#include "Figures/CHexagon.h"
+#include "Figures/CRectangle.h"
 #include "Figures/CCircle.h"
+#include "Figures/CTriangle.h"
+#include "Figures/CSquare.h"
+#include "Figures/CHexagon.h"
+#include <iostream>
 #include <fstream>
 #include "GUI\UI_Info.h"
 #include "DEFS.h"
@@ -20,72 +20,64 @@ void LoadAction::ReadActionParameters() {
 	
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
-	pOut->PrintMessage("Load Figures: Click to load");
-	pIn->GetPointClicked(P1.x, P1.y);
+	pOut->PrintMessage("Load: Enter file name: ");
+	FileName = pIn->GetSrting(pOut);
 	pOut->ClearStatusBar();
 }
 
+void LoadAction::Execute(bool readparameters ) {
 
-
-void LoadAction::Execute() {
-
-	string FigColor;
-
-
-	//This action needs to read some parameters first
-	ReadActionParameters();
 	Output* pOut = pManager->GetOutput();
-	Input* pIn = pManager->GetInput();
-	string tempColor;
+	
+	ReadActionParameters();
+	
+	//Input* pIn = pManager->GetInput();
+	
+	LoadFile.open(FileName, ios::in);
 
-	LoadFile.open("Figures.txt", ios::out);
 	if (!LoadFile.is_open()) {
 		cout << "Error opening file" << endl;
 	}
-	else {
-		
-		cout << "File opened successfully" << endl;
-		LoadFile << incFig << endl;
-		for (int i = 0; i < 200; i++) {
-			CFigure* pFig = pManager->GetFigure(P1.x, P1.y);
-			
-			if (pFig != nullptr) {
-				if (pFig->getColor() == RED) {
-					tempColor = "Red";
-				}
-				else if (pFig->getColor() == BLUE) {
-					tempColor = "Blue";
-				}
-				else if (pFig->getColor() == YELLOW) {
-					tempColor = "Yellow";
-				}
-				else if (pFig->getColor() == BLACK) {
-					tempColor = "Black";
-				}
-				else if (pFig->getColor() == ORANGE) {
-					tempColor = "Orange";
-				}
-				else if (pFig->getColor() == GREEN) {
-					tempColor = "Green";
-				}
 
-				pFig->Load(LoadFile);
-				pFig->setID(i);
-				LoadFile << pFig->getFigureType()<< "\t" << pFig->getID() << endl;
+	string drawColor, fillColor;
+	LoadFile >> drawColor >> fillColor; //reading draw and fill colors
 
-			}
+	color DrawColor = pManager->StringToColor(drawColor);
+	pOut->setCrntDrawColor(DrawColor);
 
+	if (fillColor == "No_Fill")
+		pOut->setCrntFillColor(false);
+	else
+	{
+		color FillColor = pManager->StringToColor(fillColor);
+		pOut->setCrntFillColor(FillColor);
+		pOut->setCrntFillColor(true);
+	}
+	string figureType;
+	while (LoadFile >> figureType) //reading figure type
+	{
+		CFigure* pFig = nullptr;
+
+		if (figureType == "RECT")
+			pFig = new CRectangle();
+		else if (figureType == "CIRC")
+			pFig = new CCircle();
+		else if (figureType == "SQR")
+			pFig = new CSquare();
+		else if (figureType == "TRI")
+			pFig = new CTriangle();
+		else if (figureType == "HEX")
+			pFig = new CHexagon();
+
+		if (pFig)
+		{
+			pFig->Load(LoadFile); 
+			pManager->AddFigure(pFig); // Add to ApplicationManager
 		}
 	}
-}
-LoadAction::~LoadAction() {
-	LoadFile.close();
-	//Destructor
-	pManager->RemoveAction(this);
-	pManager->UpdateInterface();
-	pManager->GetOutput()->ClearStatusBar();
-	pManager->GetInput()->GetPointClicked(P1.x, P1.y);
-	pManager->GetOutput()->ClearStatusBar();
+
 	
-	delete this;
+	LoadFile.close();
+	pOut->PrintMessage("Graph loaded successfully!");
 }
+LoadAction::~LoadAction() {}
