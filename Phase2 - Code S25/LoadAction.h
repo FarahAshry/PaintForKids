5d3078a8ff62e9aaeeb8014519d86bdf3c96f88e
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-05 02:18:55 +0300

    Update LoadAction.h

--- Phase2 - Code S25/LoadAction.h
+++ Phase2 - Code S25/LoadAction.h
@@ -1,19 +1,17 @@
 #pragma once
 #include <fstream>
-#include "D:\Uni\Semester 2\Programming Techniques\Project\PT Project Phase1 - Code S25\Phase2 - Code S25\Actions\Action.h"
+#include "Actions\Action.h"
 
 class LoadAction :public Action
 {
 private:
-	std::ofstream LoadFile; //File to load from
+	std::ifstream LoadFile; //File to load from
+	string FileName;
 
-
-	Point P1; //Point to load from
 public:
-	LoadAction(ApplicationManager* pApp, int incFig); //Constructor
+	LoadAction(ApplicationManager* pApp); //Constructor
 	void ReadActionParameters(); //Reads parameters required for action to execute
-	void Execute(); //Execute action (code depends on action type)
-	virtual void Load(ofstream &Infile);
+	void Execute(bool readparameters = true); //Execute action (code depends on action type)
 	~LoadAction(); //Destructor
 };
 
