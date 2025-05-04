d8914ff23ac4a085381bdf0421e516f0e4abe2f2
daiKhaled <dai.abdelsalam07@eng-st.cu.edu.eg>
2025-05-04 21:04:30 +0300

    Update CSquare.h

--- Phase2 - Code S25/Figures/CSquare.h
+++ Phase2 - Code S25/Figures/CSquare.h
@@ -10,8 +10,9 @@ private:
 	double length;
 public:
 	CSquare(Point, double, GfxInfo FigureGfxInfo);
-	virtual void Draw(Output* pOut) const;
-	CFigure* Clone() const;
+virtual void Load(ofstream& Infile);
+virtual CFigure* Clone() const;
+virtual void paste_clone(Point c);
 };
 
 #endif
