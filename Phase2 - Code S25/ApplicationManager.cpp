44f839cdc3e39d358b93797e40a58d0fa828bf6f
Nadarkk <nada.abdelhalim07@eng-st.cu.edu.eg>
2025-05-05 21:59:57 +0300

    Update ApplicationManager.cpp

--- Phase2 - Code S25/ApplicationManager.cpp
+++ Phase2 - Code S25/ApplicationManager.cpp
@@ -322,3 +322,4 @@ int ApplicationManager::GetSelectedFigureCount(FigureType type) const {
 CFigure* ApplicationManager::GetFigureList() const { return FigList; }
 
 int ApplicationManager::GetFigureCount() const { return FigCount;}
+//a
