8e133ac76627ff06dd5eabe0977c7d779724e942
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-05 14:56:22 +0300

    Update CTriangle.h

--- Phase2 - Code S25/Figures/CTriangle.h
+++ Phase2 - Code S25/Figures/CTriangle.h
@@ -15,6 +15,7 @@ virtual void Draw(Output* pOut) const;
 virtual void Load(ifstream& Infile);
 virtual CFigure* Clone() const;
 virtual void paste_clone(Point c);
+virtual void printInfo()const;
 virtual void save(ofstream& OutFile) const;
 };
 
