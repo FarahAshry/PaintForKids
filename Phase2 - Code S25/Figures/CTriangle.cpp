bcfc062d01febcedcb94d13e6d2bb8bdee7604eb
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-05 11:09:04 +0300

    Update CTriangle.cpp

--- Phase2 - Code S25/Figures/CTriangle.cpp
+++ Phase2 - Code S25/Figures/CTriangle.cpp
@@ -61,9 +61,9 @@ void CTriangle::save(ofstream& OutFile) const
                << Corner1.x << '\t' << Corner1.y << '\t'
                << Corner2.x << '\t' << Corner2.y << '\t'
 	       << Corner3.x << '\t' << Corner3.y << '\t'
-    	       << FigGfxInfo.DrawClr << '\t';
+    	       << colourString(FigGfxInfo.DrawClr) << '\t';
     
-    if(FigGfxInfo.isFilled) OutFile << FigGfxInfo.FillClr;
+    if(FigGfxInfo.isFilled) OutFile << colourString(FigGfxInfo.FillClr);
     else OutFile << "NO_FILL";
 	OutFile << '\n';
 }
