454b854c6f29f189c1621b2ebb20cbe940b36d62
Nadarkk <nada.abdelhalim7@eng-st.cu.edu.eg>
2025-05-05 20:46:34 +0300

    swap

--- Phase2 - Code S25/Actions/Swap.cpp
+++ Phase2 - Code S25/Actions/Swap.cpp
@@ -10,7 +10,23 @@ Swap::Swap(ApplicationManager* pApp) :Action(pApp) {
 
 }
 void Swap::ReadActionParameters() {
+	Output* pOut = pManager->GetOutput();
+	int countS = 0;
+	countS = pManager->GetSelectedCount();
+	if (countS < 2) {
+		pOut->PrintMessage("Please select atleast 2 figures to swap");
+		countS = 0;
+	
+		
+	}
+	else if (countS > 2)
+	{
+		pOut->PrintMessage("Select only 2 figures to swap");
+	}
+	else {
+		pOut->PrintMessage("Selected 2 figures to swap");
 
+	}
 }
 void Swap::Execute() {
 
