6f91d8c8d90bfb539381cbf03db81abab94cb748
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-05 22:32:03 +0300

    Update ApplicationManager.h

--- Phase2 - Code S25/ApplicationManager.h
+++ Phase2 - Code S25/ApplicationManager.h
@@ -56,9 +56,7 @@ public:
 	void UpdateInterface() const;	//Redraws all the drawing window	
 	
 	CFigure* RemoveAction(Action* pAct);
-	
-	void LoadAll();
-	
+
 	void RemoveFig(CFigure* pFig);
 	
 	CFigure* GetSelected() const;
@@ -72,6 +70,8 @@ public:
 	void Uncut();
 
 	void saveAll(ofstream& file) const;
+
+	void LoadAll(ifstream& Infile);
 
 	bool GetIsCut();
 
