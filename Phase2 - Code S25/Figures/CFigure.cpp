d83e88f866a45b96be9f05adb6aa5fef1aa46569
daiKhaled <dai.abdelsalam07@eng-st.cu.edu.eg>
2025-05-05 10:27:31 +0300

    Update CFigure.cpp

--- Phase2 - Code S25/Figures/CFigure.cpp
+++ Phase2 - Code S25/Figures/CFigure.cpp
@@ -93,3 +93,19 @@ bool CFigure::IsFilled()const
 {
 	return FigGfxInfo.isFilled; 
 }
+color CFigure::StringToColor(string color) {
+	if (color == "RED")
+		return RED;
+	else if (color == "GREEN")
+		return GREEN;
+	else if (color == "BLUE")
+		return BLUE;
+	else if (color == "YELLOW")
+		return YELLOW;
+	else if (color == "BLACK")
+		return BLACK;
+	else if (color == "WHITE")
+		return WHITE;
+	else
+		return WHITE; // The function must return a color so let white be the default
+}
