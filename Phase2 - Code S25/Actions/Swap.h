3dff640fad0cbdcbc250b18f9409b1feb6ab5a8f
Nadarkk <nada.abdelhalim07@eng-st.cu.edu.eg>
2025-05-05 14:39:48 +0300

    Update Swap.h

---
+++ Phase2 - Code S25/Actions/Swap.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Action.h"        
+#include "../ApplicationManager.h" 
+#include "../GUI/Output.h"
+
+class Swap : public Action
+{
+	Swap(ApplicationManager* pApp);
+	void ReadActionParameters();
+	void Execute();
+};
