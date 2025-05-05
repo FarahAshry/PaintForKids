47e61d38c4d65902ea9ba6438e85b6f457da902f
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-05 18:17:12 +0300

    Update CCircle.h

--- Phase2 - Code S25/Figures/CCircle.h
+++ Phase2 - Code S25/Figures/CCircle.h
@@ -10,12 +10,12 @@ private:
 	double radius;
 public:
 	CCircle(Point, double, GfxInfo FigureGfxInfo);
-virtual void Draw(Output* pOut) const;
-virtual void Load(ifstream& Infile);
-virtual CFigure* Clone() const;
-virtual void paste_clone(Point c);
-virtual void printInfo()const;
-virtual void save(ofstream& OutFile) const;
+	virtual void Draw(Output* pOut) const;
+	virtual void Load(ifstream& Infile);
+	virtual CFigure* Clone() const;
+	virtual void paste_clone(Point c);
+	virtual void printInfo()const;
+	virtual void save(ofstream& outputFile) const;
 };
 
 #endif
