7a344d730d6749bffb7de52da75f5265bf4cf94d
FarahAshry <Farah.Bekhet05@eng-st.cu.edu.eg>
2025-05-05 22:27:58 +0300

    Update CSquare.h

--- Phase2 - Code S25/Figures/CSquare.h
+++ Phase2 - Code S25/Figures/CSquare.h
@@ -17,6 +17,7 @@ public:
 	virtual void save(ofstream& outputFile) const;
 	virtual void Load(ifstream& Infile);
 	virtual color StringToColor(string color);
+	virtual FigureType getFigureType() const override;
 };
 
 #endif
