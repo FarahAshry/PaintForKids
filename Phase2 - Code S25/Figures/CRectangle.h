bda39d5e617c2c371889c9a312282259ddef7d86
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-05 18:16:23 +0300

    Update CRectangle.h

--- Phase2 - Code S25/Figures/CRectangle.h
+++ Phase2 - Code S25/Figures/CRectangle.h
@@ -16,6 +16,6 @@ public:
 	virtual CFigure* Clone() const;
 	virtual void paste_clone(Point c);
 	virtual void printInfo()const;
-	virtual void save(ofstream& OutFile) const;
+	virtual void save(ofstream& outputFile) const;
 };
 #endif
