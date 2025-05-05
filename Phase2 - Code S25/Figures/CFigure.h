3935d58d91551613516086052d50371691bf3e5a
mayaaboshadi <mayamoatasem@gmail.com>
2025-05-05 14:40:27 +0300

    Update CFigure.h

--- Phase2 - Code S25/Figures/CFigure.h
+++ Phase2 - Code S25/Figures/CFigure.h
@@ -30,7 +30,7 @@ public:
 
 	//function for SaveAction
 	virtual void save(ofstream& OutFile) const = 0;
-	
+	virtual void printInfo() const = 0;
 
 	///The following functions should be supported by the figure class
 	///It should be overridden by each inherited figure
