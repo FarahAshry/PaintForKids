94cd68661e112934d45b6f2457f2968b76693d75
Maya Aboshadi <mayaaboshadii@gmail.com>
2025-05-06 04:57:01 +0300

    last

--- Phase2 - Code S25/ApplicationManager.cpp
+++ Phase2 - Code S25/ApplicationManager.cpp
@@ -347,7 +347,7 @@ int ApplicationManager::GetSelectedFigureCount(FigureID type) const {
 }
 
 CFigure** ApplicationManager::GetFigureList() {
-	return (CFigure**)FigList;
+	return &FigList[0];
 }
 
 int ApplicationManager::GetFigureCount() const { return FigCount;}
